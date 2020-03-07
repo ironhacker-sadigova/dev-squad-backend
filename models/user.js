1b034b13764b126256a37c9e5ef42fd65cff3e92
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-03-07 21:03:17 +0100

    7.Passport encrypting with cryptoJS

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const uuidv1 = require ('uuid/v1'); 
+var CryptoJS = require("crypto-js");
 const Schema = mongoose.Schema;
-
 const userSchema = new Schema(
     {
         name: {
@@ -27,5 +28,36 @@ const userSchema = new Schema(
         updated: Date
     }
 );
+
+userSchema.virtual('password') // in order to have non peristent data :) and save the password but not in DB 
+.set(function(password){
+    // Temporary password
+    this._password = password
+    // generating a timestamp
+    this.salt = uuidv1()
+    // and encrypt the password
+    this.hashed_Password= this.encryptPassword(password)
+})
+.get(function() {
+    return this._password;
+}) ;
+
+// Encrypted password Method to do the hashing
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {  // if successfull
+            return CryptoJS.AES.encrypt('my message', this.salt)
+            .toString(password);
+
+        } catch (err){
+            return "";
+
+        }
+    }
+
+}
+
 const User = mongoose.model('Issue', userSchema);
 module.exports = User; 
