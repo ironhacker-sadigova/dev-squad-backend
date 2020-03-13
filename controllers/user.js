aeacaa230a0541ffcb5c962100cf4548127686b2
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-03-13 23:11:51 +0100

    12 Update user with lodash methods

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+
+const _= require ('lodash'); //  a JS lib providing methods & functionalities and we always write _
 const User = require  ('../models/user');
 
 
@@ -49,4 +51,25 @@ exports.getSingleUser = (req, res) => {
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json (req.profile);
+};
+
+
+// USER PROFILE UPDATE METHOD
+
+exports.updateUser = (req, res, next)=> {
+    let user = req.profile;// first we extract user info 
+    user = _.extend(user,req.body); // lodash method extend : it changes the source object (user)
+    user.updated = Date.now();
+    // save the updated user in the DB 
+    user.save((err)=>{
+        if (err) {
+            return res.status(400).json({
+                error: 'unauthorized action'
+            });
+        }
+        // in order not to give the properties of password to the front we set it to undefined
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json ({ user });
+    });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { userById, showAllUsers, getSingleUser } = require ('../controllers/user');
+const { userById, showAllUsers, getSingleUser, updateUser } = require ('../controllers/user');
 const { signinRequire } = require("../controllers/auth");
+
+
 const router = express.Router(); // to have access to the express Router
 
 
 
 router.get ('/users', showAllUsers);
 router.get ('/user/:userId', signinRequire, getSingleUser); // you must be loged in to see the selected user
+router.put ('/user/:userId', signinRequire, updateUser); // put is to make changes & update :))
 
 router.param('userId', userById);
 
