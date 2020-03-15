d262691e617197683eebf9eae9af6fccf8c8ebbb
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-03-15 23:21:33 +0100

    15.Create post method changed with image upload

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,11 @@
 const Post = require('../models/post');
+const Formidable = require ('formidable'); // its a package to handle files
+const FileSystem = require ('fs')
 
 
 // TO GET ALL THE POSTS FROM THE DATABASE
 
-exports.getPosts = (req,res)=> {
+exports.getPosts = (req,res) => {
     const posts = Post.find()  // it will find and give all the posts normally
     .then((posts)=> {
         res.json({
@@ -16,20 +18,32 @@ exports.getPosts = (req,res)=> {
 // to create a new post
 
 exports.createPost = (req,res) => {
-    const post = new Post(req.body);
-    post.save().then(result => {
-        res.status(200).json({
-            post:result
-        });
-
-       /* if(err) {
+    let form = new Formidable.IncomingForm() // method with formidable package incoming fields
+    form.keepExtensions = true; // in order not to change the format of the file
+    form.parse(req, (err, fields, files)=> {
+        if(err){
             return res.status(400).json({
-                error: err
+                error: 'Please choose another file. Your file cannot be uploaded'
             });
         }
-        res.status(200).json({
-            post: result
-        }); */
+        let post = new Post(fields) ; // its like a req.body 
+        post.postedBy = req.profile ; // req.profile has the user info and the post will be by that user
+        if (files.photo){ // it will come from front-end client, once we handle the files :
+            post.photo.data = FileSystem.readFileSync(files.photo.path); // will read the photo path
+            post.photo.contentFormat = files.photo.type; // we populate the model
+
+        }
+
+        post.save((err,result) => {
+            if (err){
+                return res.status(400).json({
+                    error:err
+                });
+            }
+            res.json({
+                post:result
+            });
+        });
 
     });
 
--- models/post.js
+++ models/post.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+// Need to create a relationship between posts & users
 
 const postSchema = new Schema(
     {
@@ -12,6 +14,19 @@ const postSchema = new Schema(
             type: String,
             required: true,
             maxlength:'6000'
+        },
+        photo: {
+            data: Buffer, // when we upload an image it is a space in the buffer where we will store the image till its loaded
+            contentFormat: String
+        },
+        postedBy:{
+            type: mongoose.Schema.ObjectId, // will be determined by the type we give
+            ref: 'User'
+
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
         }
     }
 );
