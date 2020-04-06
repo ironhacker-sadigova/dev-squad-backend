6660dedc3b818986fe2053e0959d00ac1dd1e70c
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-04-06 18:14:24 +0200

    refactored

--- app.js
+++ app.js
@@ -130,22 +130,21 @@ mongoose.connection.on('error', err => {
 
 
 // bring in routes
-const postRoutes = require("./routes/post");
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
+const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 // apiDocs
-app.get("/", (req, res) => {
-    fs.readFile("docs/apiDocs.json", (err, data) => {
-        if (err) {
-            res.status(400).json({
-                error: err
-            });
-        }
-        const docs = JSON.parse(data);
-        res.json(docs);
-    });
+app.get('/api', (req, res) => {
+  fs.readFile('docs/apiDocs.json', (err, data) => {
+      if (err) {
+          res.status(400).json({
+              error: err
+          });
+      }
+      const docs = JSON.parse(data);
+      res.json(docs);
+  });
 });
-
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
@@ -173,5 +172,5 @@ app.use(function(err, req, res, next) {
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`A Node Js API is listening on port: ${port}`);
+    console.log(`API is listening on port: ${port}`);
 });
--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,4 @@
-
-
-const express = require("express");
+const express = require('express');
 const {
     getPosts,
     createPost,
@@ -8,33 +6,37 @@ const {
     postById,
     isPoster,
     updatePost,
-    deletePost
-} = require("../controllers/post");
-const { requireSignin } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
-const { createPostValidator } = require("../validator");
+    deletePost,
+    photo,
+    singlePost,
+    like,
+    unlike,
+    comment,
+    uncomment,
+    updateComment
+} = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { createPostValidator } = require('../validator');
 
 const router = express.Router();
 
-router.get("/posts", getPosts);
-router.post(
-    "/post/new/:userId", // to create a new post user should be signed in
-    requireSignin,
-    createPost,
-    createPostValidator
-);
-router.get("/posts/by/:userId", requireSignin, postsByUser);
-router.put("/post/:postId", requireSignin, isPoster, updatePost);
-router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+router.get('/posts', getPosts);
+
+
+
+// post routes
+router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
+router.get('/posts/by/:userId', requireSignin, postsByUser);
+router.get('/post/:postId', singlePost);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+// photo
+router.get('/post/photo/:postId', photo);
 
 // any route containing :userId, our app will first execute userById()
-router.param("userId", userById);
-// Making a query to the DB & getting userInfo & appending to the user object
- // Need to create the userInfobyID method in the controller
- // Any route with ':userId,' the app will execute userInfoByID()
-
-
+router.param('userId', userById);
 // any route containing :postId, our app will first execute postById()
-router.param("postId", postById);
+router.param('postId', postById);
 
 module.exports = router;
