487e26bb3b6709aa0e9f3bb6049432ff85962634
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-04-07 16:09:13 +0200

    few changes

--- validator/index.js
+++ validator/index.js
@@ -5,13 +5,13 @@
 
     exports.createPostValidator = (req, res, next) => {
         req.check('title', 'Write a title').notEmpty();
-        req.check('title', 'Title must be between 4 to 150 characters').isLength({
-            min: 4,
-            max: 150
+        req.check('title', 'Title must be between 20 to 100 characters').isLength({
+            min: 20,
+            max: 100
         });
         req.check('body', 'Write a body').notEmpty();
-        req.check('body', 'Body must be between 4 to 2000 characters').isLength({
-            min: 4,
+        req.check('body', 'Body must be between 100 to 2000 characters').isLength({
+            min: 100,
             max: 2000
         });
         const errors = req.validationErrors();
